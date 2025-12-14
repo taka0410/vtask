d1fa3e9eb390038e5f51b1f361b99b1fb5c2a4da
taka0410 <t.mazz0410@gmail.com>
2025-12-14 22:44:07 +0900

    fix next config eslint setting

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
+// next.config.ts
+
+// この import は残しても消してもどちらでもOK（消してもよい）
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
   eslint: {
     // ビルド時に ESLint エラーがあってもデプロイは通す
